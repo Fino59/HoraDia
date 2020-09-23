c4ce6f8725176cfcf13118d1e3108370df08ef5f
Danilo Fontes <57161150+Fino59@users.noreply.github.com>
2020-09-23 15:23:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ function carregar() {
     
     if (hora >= 0 && hora <12) {
         msg.innerHTML = `Bom dia, agora são ${hora} horas.`
-        img.src = '../img/fotoamanhecer.png' // Bom dia
+        img.src = './img/fotoamanhecer.png' // Bom dia
         document.body.style.background = '#EBE330'
     } else if (hora > 12 && hora <18) {
         msg.innerHTML = `Boa tarde, agora são ${hora} horas.`
-        img.src = '../img/fototarde.png' //Boa Tarde
+        img.src = './img/fototarde.png' //Boa Tarde
         document.body.style.background = '#FDA240'
     } else {
         msg.innerHTML = `Boa noite, agora são ${hora} horas.`
-        img.src = '../img/fotonoite.png' //Boa Noite
+        img.src = './img/fotonoite.png' //Boa Noite
         document.body.style.background = '#033739'
     }
 }
