48acea35c5c00e136953bb61684180c9f6253dfb
Danilo Fontes <slipdan@hotmail.com>
2021-09-03 14:25:39 -0300

    Update function with minutes

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    let minutes = data.getMinutes();
     
     if (hora >= 5 && hora < 12) {
-        msg.innerHTML = `Bom dia, agora são ${hora} horas.`
-        img.src = './img/fotoamanhecer.png' // Bom dia
-        document.body.style.background = '#f7f381'
+        msg.innerHTML = `Bom dia, agora são ${hora}h${minutes}min.`;
+        img.src = './img/fotoamanhecer.png';
+        document.body.style.background = '#f7f381';
     } else if (hora >= 12 && hora < 18) {
-        msg.innerHTML = `Boa tarde, agora são ${hora} horas.`
-        img.src = './img/fototarde.png' //Boa Tarde
-        document.body.style.background = '#FDA240'
+        msg.innerHTML = `Boa tarde, agora são ${hora}h${minutes}min.`;
+        img.src = './img/fototarde.png';
+        document.body.style.background = '#FDA240';
     } else {
-        msg.innerHTML = `Boa noite, agora são ${hora} horas.`
-        img.src = './img/fotonoite.png' //Boa Noite
-        document.body.style.background = '#033739'
+        msg.innerHTML = `Boa noite, agora são ${hora}h${minutes}min.`;
+        img.src = './img/fotonoite.png';
+        document.body.style.background = '#033739';
     }
 }
