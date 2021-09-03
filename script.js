a48af6972e2b05d8965474392a4709a294a06b6f
Danilo Fontes <slipdan@hotmail.com>
2021-09-03 13:09:10 -0300

    Correct error on getHours function

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     
-    if (hora >= 5 && hora <12) {
+    if (hora >= 5 && hora < 12) {
         msg.innerHTML = `Bom dia, agora são ${hora} horas.`
         img.src = './img/fotoamanhecer.png' // Bom dia
         document.body.style.background = '#f7f381'
-    } else if (hora > 12 && hora <18) {
+    } else if (hora >= 12 && hora < 18) {
         msg.innerHTML = `Boa tarde, agora são ${hora} horas.`
         img.src = './img/fototarde.png' //Boa Tarde
         document.body.style.background = '#FDA240'
